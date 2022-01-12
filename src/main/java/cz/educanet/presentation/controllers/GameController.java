89d392fe2b19e831bf5290d367ec09a9637b7db0
Mates0 <matyas.vanko@educanet.cz>
2022-01-12 22:28:40 +0100

    OnMouseClicked changing color

--- src/main/java/cz/educanet/presentation/controllers/GameController.java
+++ src/main/java/cz/educanet/presentation/controllers/GameController.java
@@ -4,8 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
-import java.awt.event.ActionEvent;
-
 public class GameController {
     private final int gridSize = 8;
     @FXML
@@ -14,10 +12,13 @@ public class GameController {
     public void initialize() {
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
-                Button b = new Button();
-                b.setPrefWidth(200);
-                b.setPrefHeight(200);
-                grid.add(b, i+3,j+3);
+                Button btn = new Button();
+                btn.setPrefWidth(200);
+                btn.setPrefHeight(200);
+                btn.setOnMouseClicked(event -> {
+                    btn.setStyle("-fx-background-color: green");
+                });
+                grid.add(btn, i+3,j+3);
             }
         }
     }
