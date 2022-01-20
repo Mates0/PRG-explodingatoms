a483239dfae13ee9ab3f8a1321c192724900e271
Mates0 <matyas.vanko@educanet.cz>
2022-01-20 22:22:27 +0100

    circles added v2

--- src/main/java/cz/educanet/presentation/controllers/GameController.java
+++ src/main/java/cz/educanet/presentation/controllers/GameController.java
@@ -13,30 +13,27 @@ public class GameController {
     public GridPane grid;
 
     public void initialize() {
+        Image bluecircle1 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/bluecircle1.png");
+        Image bluecircle2 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/bluecircle2.png");
+        Image bluecircle3 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/bluecircle3.png");
+        Image redcircle1 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/redcircle1.png");
+        Image redcircle2 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/redcircle2.png");
+        Image redcircle3 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/redcircle3.png");
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
-                Image bluecircle1 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/bluecircle1.png");
-                Image bluecircle2 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/bluecircle2.png");
-                Image bluecircle3 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/bluecircle3.png");
-                Image redcircle1 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/redcircle1.png");
-                Image redcircle2 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/redcircle2.png");
-                Image redcircle3 = new Image("file:/C:/Users/maste/IdeaProjects/PRG-explodingatoms/redcircle3.png");
                 Button btn = new Button();
-                btn.setPrefWidth(200);
-                btn.setPrefHeight(200);
+                btn.setPrefWidth(250);
+                btn.setPrefHeight(250);
                 btn.setOnMouseClicked(event -> {
                     ImageView view = new ImageView();
-                    if (isRed == true) {
+                    view.setFitHeight(50);
+                    view.setPreserveRatio(true);
+                    btn.setGraphic(view);
+                    if (isRed) {
                         view.setImage(redcircle1);
-                        view.setFitHeight(50);
-                        view.setPreserveRatio(true);
-                        btn.setGraphic(view);
                         isRed = false;
                     } else {
                         view.setImage(bluecircle1);
-                        view.setFitHeight(50);
-                        view.setPreserveRatio(true);
-                        btn.setGraphic(view);
                         isRed = true;
                     }
                 });
